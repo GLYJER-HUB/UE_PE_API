4eb4b130b74a6e3063efd3cd2301cac3e42878fa
jcblanc2 <jclaytonblanc@gmail.com>
2024-01-15 11:26:06 -0500

    Add static files

--- src/app.js
+++ src/app.js
@@ -1,53 +1,52 @@
-require('dotenv/config');
-const express = require('express');
-const cors = require('cors');
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const userLogRoutes = require('./routes/userLogRoutes');
-const projectRoutes = require('./routes/projectRoutes');
-const cookieParser = require('cookie-parser');
-const mongoose = require('mongoose');
-const createDefaultUsers = require('./utils/userSeed');
+require("dotenv/config");
+const express = require("express");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const userLogRoutes = require("./routes/userLogRoutes");
+const projectRoutes = require("./routes/projectRoutes");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const createDefaultUsers = require("./utils/userSeed");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Connect to the database
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('Connected to database');
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to database");
 
-        // Create default admin and superadmin users if they don't exist
-        await createDefaultUsers();
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
-}
+    // Create default admin and superadmin users if they don't exist
+    await createDefaultUsers();
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors({
-    credentials: true, origin: [
-        "http://localhost:3000",
-        "http://localhost:5173",
-        "*"
-    ]
-}));
-app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:3000", "http://localhost:5173", "*"],
+  })
+);
+app.use("/files", express.static(__dirname + "/../uploads"));
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/logs', userLogRoutes);
-app.use('/api/projects', projectRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/logs", userLogRoutes);
+app.use("/api/projects", projectRoutes);
 
 // Launch the server
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`It's alive on http://localhost:${PORT}`)
-    })
-})
+  app.listen(PORT, () => {
+    console.log(`It's alive on http://localhost:${PORT}`);
+  });
+});
