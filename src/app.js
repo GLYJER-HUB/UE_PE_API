0b74d93b5e7a554044f2620e64da2f0e57067b29
John Clayton Blanc <108766949+jcblanc2@users.noreply.github.com>
2024-01-15 10:43:09 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require('dotenv/config');
 const express = require('express');
 const cors = require('cors');
+const path = require('path')
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const userLogRoutes = require('./routes/userLogRoutes');
@@ -37,7 +38,7 @@ app.use(cors({
         "*"
     ]
 }));
-app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 // Routes
 app.use('/api/auth', authRoutes);
