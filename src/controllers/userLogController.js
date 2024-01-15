d705e320342ae93fe716f90905d4f20a825b687c
jcblanc2 <jclaytonblanc@gmail.com>
2024-01-15 11:50:15 -0500

    Get logs

--- src/controllers/userLogController.js
+++ src/controllers/userLogController.js
@@ -1,34 +1,50 @@
-const userLogModel = require('../models/userLogModel');
-require('dotenv/config');
+const userLogModel = require("../models/userLogModel");
+require("dotenv/config");
+
+// Controller to get userLogs
+const getLogsController = async () => {
+  try {
+    // Retrieve all userlogs from the database
+    const allLogs = await userLogModel
+      .find()
+      .populate("user_id", "username")
+      .sort({ last_login: -1 });
+
+    res.status(200).send({ logs: allLogs });
+  } catch (error) {
+    console.error("Error getting all user logs:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 
 // Controller to add a new userLog
 const addLogController = async (userId, loginTime, logoutTime) => {
-    try {
-        // Create a new userLog document
-        const newUserLog = new userLogModel({
-            user_id: userId,
-            last_login: loginTime,
-            last_logout: logoutTime,
-        });
+  try {
+    // Create a new userLog document
+    const newUserLog = new userLogModel({
+      user_id: userId,
+      last_login: loginTime,
+      last_logout: logoutTime,
+    });
 
-        // Save the document to the database
-        await newUserLog.save();
-        return newUserLog._id
-    } catch (error) {
-        console.error('Error adding UserLog:', error);
-    }
-}
-
+    // Save the document to the database
+    await newUserLog.save();
+    return newUserLog._id;
+  } catch (error) {
+    console.error("Error adding UserLog:", error);
+  }
+};
 
 // Controller to update a userLog
 const updateLogController = async (logId, logoutTime) => {
-    try {
-        await userLogModel.findOneAndUpdate(
-            { _id: logId },
-            { last_logout: logoutTime });
-    } catch (error) {
-        console.error('Error updating userLog:', error);
-    }
-}
+  try {
+    await userLogModel.findOneAndUpdate(
+      { _id: logId },
+      { last_logout: logoutTime }
+    );
+  } catch (error) {
+    console.error("Error updating userLog:", error);
+  }
+};
 
-module.exports = { addLogController, updateLogController };
+module.exports = { getLogsController, addLogController, updateLogController };
--- src/routes/userLogRoutes.js
+++ src/routes/userLogRoutes.js
@@ -1,14 +1,18 @@
-const router = require('express').Router();
-const verifyToken = require('../middlewares/jwtVerification');
+const router = require("express").Router();
+const verifyToken = require("../middlewares/jwtVerification");
 const {
-    addLogController,
-    updateLogController
-} = require('../controllers/userLogController');
+  addLogController,
+  updateLogController,
+  getLogsController,
+} = require("../controllers/userLogController");
+
+// Endpoint to get all logs
+router.get("/", verifyToken, getLogsController);
 
 // Endpoint to add a log
-router.post('/', verifyToken, addLogController);
+router.post("/", verifyToken, addLogController);
 
 // Endpoint to update a log
-router.put('/:id', verifyToken, updateLogController);
+router.put("/:id", verifyToken, updateLogController);
 
 module.exports = router;
